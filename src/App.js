30041037e0d6ee43d6ddfc5ce74e100b7eae7919
Devesh <devesh12200@gmail.com>
2023-02-17 23:55:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import IndProduct from './pages/IndProduct';
+import Login from './pages/Login';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import {
+  HashRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 
 function App() {
+  // initially the user would not be logged in, ie there would be no user, so user = false
+  const user = false
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <HashRouter basename='/'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            
+            <Route path="/products/:category" element={<ProductList />} />
+            
+            <Route path="/product/:id" element={<IndProduct />} />
+            
+            <Route path="/cart" element={<Cart />} />
+            
+            <Route path="/login" element={
+              //if a user is logged in, the /login will redirect to home page ie /
+              user ? (<Navigate replace to={"/"} />) : (<Login />)
+            } />
+            
+            <Route path="/register" element={
+              user ? (<Navigate replace to={"/"} />) : (<Register />)
+            } />
+          </Routes>
+        </HashRouter>  
+    </>
   );
 }
 
